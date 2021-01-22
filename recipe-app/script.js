71bdaf8c53c0ac9478e34426d6683a1274fc3963
suinautant <suinautant@gmail.com>
2021-01-22 22:13:46 +0900

    recipe app

--- recipe-app/script.js
+++ recipe-app/script.js
@@ -1,5 +1,11 @@
-const meals = document.getElementById("meals");
+const mealsEl = document.getElementById("meals");
 const favoriteContainer = document.getElementById("fav-meals");
+const mealPopup = document.getElementById("meal-popup");
+const mealInfoEl = document.getElementById("meal-info");
+const popupCloseBtn = document.getElementById("close-popup");
+
+const searchTerm = document.getElementById("search-term");
+const searchBtn = document.getElementById("search");
 
 getRandomMeal();
 fetchFavMeals();
@@ -26,9 +32,16 @@ async function getMealById(id) {
 }
 
 async function getMealsBySearch(term) {
-    const meals = await fetch(
+    const resp = await fetch(
         "https://www.themealdb.com/api/json/v1/1/search.php?s=" + term
     );
+
+    const respData = await resp.json();
+    const meals = respData.meals;
+
+    console.log(meals);
+
+    return meals;
 }
 
 function addMeal(mealData, random = false) {
@@ -68,7 +81,11 @@ function addMeal(mealData, random = false) {
         fetchFavMeals();
     });
 
-    meals.appendChild(meal);
+    meal.addEventListener("click", () => {
+        showMealInfo(mealData);
+    });
+
+    mealsEl.appendChild(meal);
 }
 
 function addMealLS(mealId) {
@@ -125,5 +142,73 @@ function addMealFav(mealData) {
         fetchFavMeals();
     });
 
+    favMeal.addEventListener("click", () => {
+        showMealInfo(mealData);
+    });
+
     favoriteContainer.appendChild(favMeal);
 }
+
+function showMealInfo(mealData) {
+    // clean Meal info
+    mealInfoEl.innerHTML = "";
+
+    // update the Meal info
+    const mealEl = document.createElement("div");
+
+    const ingredients = [];
+
+    // get ingredients and measures
+    for (let i = 1; i < 20; i++) {
+        if (mealData["strIngredient" + i]) {
+            ingredients.push(`${mealData["strIngredient" + i]} -
+            ${mealData["strMeasure" + i]}`);
+            console.log(ingredients);
+        } else {
+            break;
+        }
+    }
+
+    mealEl.innerHTML = `
+        <h1>${mealData.strMeal}</h1>
+        <img
+            src="${mealData.strMealThumb}"
+            alt="${mealData.strMeal}"
+        />
+        <p>
+            ${mealData.strInstructions}
+        </p>
+        <h3>Ingredients:</h3>
+        <ul>
+            ${ingredients
+                .map(
+                    (ing) => `
+            <li>${ing}</li>
+            `
+                )
+                .join("")}
+    `;
+
+    mealInfoEl.appendChild(mealEl);
+
+    // hsow the popup
+    mealPopup.classList.remove("hidden");
+}
+
+searchBtn.addEventListener("click", async () => {
+    // clean container
+    mealsEl.innerHTML = "";
+
+    const search = searchTerm.value;
+    const meals = await getMealsBySearch(search);
+
+    if (meals) {
+        meals.forEach((meal) => {
+            addMeal(meal);
+        });
+    }
+});
+
+popupCloseBtn.addEventListener("click", () => {
+    mealPopup.classList.add("hidden");
+});
